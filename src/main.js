5a661f28f72179f6ef43b006c3384767a13a1ce6
nimamesbah <nimamesbah85@gmail.com>
2024-11-22 15:36:28 +0330

    unhide function set.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-        
+const items = document.querySelectorAll(".items");
+function itemClick(evt){
+    evt.target.classList.remove("hide")
+    evt.target.classList.add("disabled")
+    console.log(evt);
+    
+    
+}
+function hideAll(){
+    for (const item of items) {
+        item.classList.add("hide")
+    }
+}
+
+setTimeout(hideAll,3000)
+
+for (const item of items) {
+    item.addEventListener("click",itemClick)
+    
+}
